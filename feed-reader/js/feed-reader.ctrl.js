1f28440a5562d0e52d806b3daf28f419375dcf5c
DaBitNG <davidnunogarcia@gmail.com>
2016-05-30 20:16:51 +0200

    Comments added

--- feed-reader/js/feed-reader.ctrl.js
+++ feed-reader/js/feed-reader.ctrl.js
@@ -1,21 +1,36 @@
 (function() {
 	
     'use strict';
- 
+ 	
+ 	/**
+ 	 * Feed Reader Module Controller
+ 	 */
     angular.module('feed-reader.controllers',['feed-reader.services'])
+    	/**
+    	 * URL of your feed
+    	 */
+    	.constant('FEED_URL','http://www.feedforall.com/feed-7.gif')
+    	/**
+    	 * Feed Reader Controller
+    	 */
+		.controller('FeedReaderCtrl', feedReader);
 
-		.controller('FeedReaderCtrl', feedReader)
-
-		function feedReader($scope, FeedReaderSrvc){
+		/**
+		 * @param  {$scope}
+		 * @param  {FeedReaderSrvc}
+		 * @param  {FEED_URL}
+		 * @return 
+		 */
+		function feedReader($scope, FeedReaderSrvc, FEED_URL){
 			// Uncomment below if you need
 			// $scope.posts = [];
-			FeedReaderSrvc.read("http://www.feedforall.com/feed-7.gif").then(function(data){
+			// Get Feed object from FeedReaderSrvc.read(url);
+			FeedReaderSrvc.read(FEED_URL).then(function(data){
 				if(data.responseData != null){
 					// Place your code here. Uncomment below if you need
 					// $scope.posts =  data.responseData.feed.entries;
 				}  
 			});	
-
 		} 
 
 })();
--- feed-reader/js/feed-reader.srvc.js
+++ feed-reader/js/feed-reader.srvc.js
@@ -1,13 +1,28 @@
 (function() {
 
     'use strict';
- 
+   
+    /**
+     * Feed Reader Module Service
+     */
     angular.module('feed-reader.services',[])
 
+        /**
+         * Google Feed API URL
+         */
     	.constant('GoogleFeedAPI', "http://ajax.googleapis.com/ajax/services/feed/load?v=1.0&num=50&callback=JSON_CALLBACK&q=")
 
+        /**
+         * Feed Reader Factory
+         */
 		.factory('FeedReaderSrvc', feedReaderFunctions);
 
+        /**
+         * @param  {$http}
+         * @param  {$q}
+         * @param  {GoogleFeedAPI}
+         * @return {function}
+         */
 		function feedReaderFunctions($http, $q, GoogleFeedAPI){
 			return {
 				read: function(url){
